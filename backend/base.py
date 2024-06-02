fea0887b98462a33ca5ca9a8a5a958daebe46c30
Vaibhav Balooni <phoenixshan16@gmail.com>
2024-06-03 01:20:32 +0530

    Master Push

---
+++ backend/base.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app=Flask(__name__)
+CORS(app)
+
+@app.route('/data',methods=['POST'])
+def submit():
+    data= request.json
+    if not data:
+        return jsonify(error="Keyword not there!"),400
+    print (data)
+    return jsonify(data)
