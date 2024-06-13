6ba94ef83b5fb9c252e39ecb792222d5d1e7290e
Vaibhav Balooni <phoenixshan16@gmail.com>
2024-06-13 18:44:27 +0530

    Latest update

--- backend/base.py
+++ backend/base.py
@@ -4,10 +4,21 @@ from flask_cors import CORS
 app=Flask(__name__)
 CORS(app)
 
-@app.route('http://localhost:5000/data',methods=['POST'])
+@app.route('/data',methods=['POST'])
 def submit():
     data= request.json
     if not data:
         return jsonify(error="Keyword not there!"),400
     print (data)
-    return jsonify(data)
+
+    with open("textdata.txt", 'r') as file:
+        multiline_string = file.read()
+    html_string=multiline_string.replace('\n','"0')
+    
+    test_string = """hello
+    fhfggfhhe
+    exxxxe"""
+    return test_string
+
+if __name__ == "__main__":
+    app(debug=True)
